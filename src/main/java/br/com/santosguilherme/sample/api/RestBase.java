ae6937c34bfa318c422fb119b2c187fce4329265
santosguilherme <sntguilherme@gmail.com>
2015-07-25 18:16:46 -0300

    Class Base for services

---
+++ src/main/java/br/com/santosguilherme/sample/api/RestBase.java
@@ -0,0 +1,18 @@
+package br.com.santosguilherme.sample.api;
+
+import br.com.santosguilherme.sample.filters.ApiFilter;
+
+/**
+ * 
+ * @author santosguilherme
+ *
+ */
+public abstract class RestBase {
+
+	protected static final String API_URL = "/api";
+	protected static final String PUBLIC_URL = "/public";
+	
+	public RestBase() {
+		new ApiFilter();
+	}
+}
